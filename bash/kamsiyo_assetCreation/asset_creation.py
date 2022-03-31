dfbaf0dac1a325fa82f265e6eb1291ec7a081540
kamsiyo-laura <99070216+kamsiyo-laura@users.noreply.github.com>
2022-03-31 21:31:54 +0100

    Add files via upload

---
+++ bash/kamsiyo_assetCreation/asset_creation.py
@@ -0,0 +1,50 @@
+
+from algosdk import account
+from algosdk.future import transaction
+
+# creator
+private_key, address = account.generate_account()
+# account that can freeze other accounts for this asset
+_, freeze = account.generate_account()
+# account able to update asset configuration
+_, manager = account.generate_account()
+# account allowed to take this asset from any other account
+_, clawback = account.generate_account()
+# account that holds reserves for this asset
+_, reserve = account.generate_account()
+
+fee_per_byte = 10
+first_valid_round = 1000
+last_valid_round = 2000
+genesis_hash = "" # enter genesis hash
+
+total = 100  # how many of this asset there will be
+assetname = "assetname"
+unitname = "unitname"
+url = "" # website url
+# should be a 32-byte hash
+metadata = bytes("", "ascii") # enter bytes
+# whether accounts should be frozen by default
+default_frozen = False
+
+# create the asset creation transaction
+sp = transaction.SuggestedParams(
+    fee_per_byte, first_valid_round, last_valid_round, genesis_hash
+)
+txn = transaction.AssetConfigTxn(
+    address,
+    sp,
+    total=total,
+    manager=manager,
+    reserve=reserve,
+    freeze=freeze,
+    clawback=clawback,
+    unit_name=unitname,
+    asset_name=assetname,
+    url=url,
+    metadata_hash=metadata,
+    default_frozen=default_frozen,
+)
+
+# sign the transaction
+signed_txn = txn.sign(private_key)
